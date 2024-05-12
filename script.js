9d46f8bc002722325b72d1d47a77411aa76abf42
Md FaizurRahman <114854264+Faizurcse@users.noreply.github.com>
2024-05-13 03:35:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+//your JS code here. If required.
+let displayTime = document.getElementById("timer");
+let currentTime = new Date();
+setInterval(
+	function(){
+		currentTime = new Date();
+		displayTime.innerHTML = currentTime.toLocaleString();
+	}, 1000
+);
